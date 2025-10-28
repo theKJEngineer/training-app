b160dca6ab72a4fede9fdd94b116447889cc5fdb
theKJEngineer <kjanas.it@gmail.com>
2025-10-28 19:58:32 +0100

    Initial project structure

---
+++ src/training/app/Main.java
@@ -0,0 +1,8 @@
+package training.app;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/training/catalog/ExerciseTemplate.java
@@ -0,0 +1,8 @@
+package training.catalog;
+
+public class ExerciseTemplate {
+    public static void main(String[] args) {
+
+
+    }
+}
