fef2f1d289a4908ae1e915c802b53e5dbe5609ac
theKJEngineer <kjanas.it@gmail.com>
2025-11-02 00:04:36 +0100

    test(app): add Main tests for Exercise class

--- src/training/app/Main.java
+++ src/training/app/Main.java
@@ -2,12 +2,14 @@ package training.app;
 
 import training.catalog.ExerciseCatalog;
 import training.catalog.ExerciseTemplate;
+import training.workout.Exercise;
 import training.workout.Set;
 
 
 public class Main {
     public static void main(String[] args) {
 
+        // test for class ExerciseTemplate:
         // Create example exercise templates
         ExerciseTemplate exerciseTemplate = new ExerciseTemplate
                 ("Squad", "Legs", "Improves leg power and stability");
@@ -19,6 +21,7 @@ public class Main {
         System.out.println(" ");
         exerciseTemplate2.info();
 
+        // test for class Exercise catalog:
         // create catalog,
         ExerciseCatalog catalog = new ExerciseCatalog(5);
         // add templates
@@ -38,6 +41,8 @@ public class Main {
         catalog.addExercise(null);
 
 
+        // test for class Set
+
         Set s1 = new Set(10, 60);
         s1.info();
         System.out.println("Total weight: " + s1.totalWeight());
@@ -47,6 +52,22 @@ public class Main {
         s2.info();
         System.out.println("Total weight: " + s2.totalWeight());
         System.out.println();
+
+        // test for class Exercise
+
+        // Test: create exercise and add sets
+        ExerciseTemplate bench = new ExerciseTemplate(
+                "Bench Press", "Chest", "Barbell press for chest strength"
+        );
+
+        Exercise benchExercise = new Exercise(bench, 3);
+
+        benchExercise.addSet(new Set(10, 60));
+        benchExercise.addSet(new Set(8, 65));
+
+        benchExercise.showExercise();
+
+        System.out.println("Total weight: " + benchExercise.getTotalWeight());
 
     }
 }
