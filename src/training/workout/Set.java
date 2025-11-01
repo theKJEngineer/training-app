1919cd8c1a51422193d108cced4c3da54d283caf
theKJEngineer <kjanas.it@gmail.com>
2025-11-01 20:10:50 +0100

    feat(workout): add Set class with validation, info() and totalWeight()

--- src/training/workout/Set.java
+++ src/training/workout/Set.java
@@ -1,4 +1,36 @@
 package training.workout;
 
 public class Set {
+    private int reps;
+    private double weight;
+
+    public Set(int reps, double weight) {
+        if (reps <= 0) {
+            System.out.println("Reps must be greater than 0. Set to 1 by default.");
+            this.reps = 1;
+        }
+        if (weight <= 0) {
+            System.out.println("Weight must be greater than 0. Set to 0 by default.");
+            this.weight = 0;
+        }
+        this.reps = reps;
+        this.weight = weight;
+    }
+
+    public int getReps() {
+        return this.reps;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    // prints set details
+    public void info() {
+        System.out.println(reps + " reps x " + weight + " kg");
+    }
+
+    public double totalWeight() {
+        return this.reps * this.weight;
+    }
 }
