a0241cfb7a8e1b75e3cf44d921d01d22ec3dbc56
theKJEngineer <kjanas.it@gmail.com>
2025-11-05 19:46:52 +0100

    feat: Add Workout class

--- src/training/workout/Workout.java
+++ src/training/workout/Workout.java
@@ -1,4 +1,55 @@
 package training.workout;
 
+/**
+ * Represents a specific exercise performed during a workout.
+ * It links an ExerciseTemplate to a list of actual Sets.
+ */
+
+
 public class Workout {
+    private Exercise[] exercises;
+    private int exerciseCount;
+    private String name;
+    private String date;
+
+    public Workout (String name, String date, int maxExercises) {
+        this.name = name;
+        this.date = date;
+        this.exercises = new Exercise[maxExercises];
+        exerciseCount = 0;
+    }
+
+    public void addExercise(Exercise e) {
+        if (exerciseCount >= exercises.length ) {
+            System.out.println("Too much exercises.");
+            return;
+        }
+        exercises[exerciseCount] = e;
+        exerciseCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void showWorkoutDetails() {
+        System.out.println("Your workout:");
+        System.out.println(getName());
+        System.out.println(getDate());
+        for (int i = 0; i < exerciseCount; i++) {
+            exercises[i].showExercise();
+        }
+    }
+
+    public double calculateTotalWorkoutWeight() {
+        double totalWeight = 0;
+        for (int i = 0; i < exerciseCount; i++) {
+            totalWeight = totalWeight + exercises[i].getTotalWeight();
+        }
+        return totalWeight;
+    }
 }
